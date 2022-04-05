c54ae67d1c7c0ebfec991b0b6afdfb4a1948d854
Anton Nazarov <anton.nazarov@quadcode.com>
2022-04-05 17:58:09 +0300

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MessageList from "./components/MessageList"
 import { AUTHOR } from './constants/main';
 
 function App() {
-  let [messageList, setMessageList] = useState([]);
-  let [message, setMessage] = useState('');
+  const [messageList, setMessageList] = useState([]);
+  const [message, setMessage] = useState('');
 
   const handleButton = () => {
-    if (message != '') {
-      const newMesage = {
+    if (message !== '') {
+      setMessageList([...messageList, {
         text: message,
         author: AUTHOR.me
-      }
-      setMessageList([...messageList, newMesage]);
+      }]);
       setMessage('');
     }
   }
 
   const handleInput = (event) => {
     setMessage(event.target.value);
-    console.log(message);
   }
+
+  useEffect(() => {
+    if (messageList.length && messageList[messageList.length - 1].author !== AUTHOR.bot) {
+      setTimeout(() => {
+        setMessageList([...messageList, {
+          text: "Привет, как дела?",
+          author: AUTHOR.bot
+        }]);
+      }, 1500)
+    }
+  }, [messageList]);
 
   return (
     <div className="App flex">
       <div className='flex left column actions'>
-        <input type="text" placeholder="enter text" value={message} onChange={handleInput} />
-        <button onClick={handleButton}>Add to list</button>
+        <input className='margin8' type="text" placeholder="enter text" value={message} onChange={handleInput} />
+        <button className='margin8' onClick={handleButton}>Add to list</button>
       </div>
-      <MessageList messages={messageList}/>
+      <MessageList messages={messageList} />
     </div>
   );
 }
